5fbf95a531d695389f75d29e5e5efbb4b4137d27
fako1024 <mail+github@f-ko.eu>
2020-10-23 08:42:20 +0200

    Add signal handler to avoid bluetooth issues when being terminated by e.g. systemd

--- cmd/brew/main.go
+++ cmd/brew/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/fako1024/brew"
 	"github.com/fako1024/brew/influx"
 	"github.com/fako1024/btscale/pkg/felicita"
@@ -13,7 +17,16 @@ func main() {
 	if err != nil {
 		logrus.StandardLogger().Fatalf("Failed to initialize Felicita scale: %s", err)
 	}
-	defer s.Close()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGTERM)
+	signal.Notify(sigChan, os.Interrupt)
+	go func() {
+		<-sigChan
+		logrus.StandardLogger().Infof("Got signal, terminating connection to scale")
+		s.Close()
+		os.Exit(0)
+	}()
 
 	influxDB := influx.NewEventTracker("http://nas.f-ko.eu:8086", "root", "root")
 	scanner := brew.NewScanner(s, influxDB)
